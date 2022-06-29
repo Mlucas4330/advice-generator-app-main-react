83de5ac0b346975ca2ca544e066deeddea13f596
Lucas Medeiros <mlucas4330@gmail.com>
2022-06-28 21:27:57 -0300

    commit

--- src/UseFetch.tsx
+++ src/UseFetch.tsx
@@ -1,34 +1,42 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function UseFetch(url: string) {
-    const [data, setData] = useState<any>(null)
-    const [loading, setLoading] = useState<boolean>(false)
-    const [error, setError] = useState<any>(null)
+    const [data, setData] = useState<any>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<any>(null);
 
     useEffect(() => {
-        setLoading(true)
-        axios.get(url).then((response) => {
-            setData(response.data)
-        }).catch((err) => {
-            setError(err)
-        }).finally(() => {
-            setLoading(false)
-        })
-    }, [url])
+        setLoading(true);
+        axios
+            .get(url)
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(err => {
+                setError(err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, [url]);
 
     const refetch = () => {
-        setLoading(true)
-        axios.get(url).then((response) => {
-            setData(response.data)
-        }).catch((err) => {
-            setError(err)
-        }).finally(() => {
-            setLoading(false)
-        })
-    }
+        setLoading(true);
+        axios
+            .get(url)
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(err => {
+                setError(err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
 
-  return {data, loading, error, refetch}
+    return { data, loading, error, refetch };
 }
 
-export default UseFetch
+export default UseFetch;
